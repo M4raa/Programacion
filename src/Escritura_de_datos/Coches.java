5d09801cdbbb00f407a6da58ef66b21f41b70278
M4raa <davidmarpons@gmail.com>
2023-12-04 11:17:30 +0100

    Reestructura

--- src/Escritura_de_datos/Coches.java
+++ src/Escritura_de_datos/Coches.java
@@ -1,4 +1,18 @@
 package Escritura_de_datos;
 
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Coches {
+    public static void main(String[] args) {
+
+        ArrayList<String> coches = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        do {
+            coches.add(sc.nextLine());
+        } while (!Objects.equals(sc.next(), ""));
+        sc.close();
+        System.out.println(coches);
+    }
 }
